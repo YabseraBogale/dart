ae26266011902e1233392eb24826b9db88341bae
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-11-13 20:11:24 +0300

    ok

--- foreach.dart
+++ foreach.dart
@@ -3,7 +3,7 @@ void main() {
   List<int> numbers = [1, 2, 3, 4, 5];
   numbers.forEach((element) {
     sum += element;
-    print(" vv The sum is increaing by $element and sum is $sum");
+    print("The sum is increaing by $element and sum is $sum");
   });
 
 }
