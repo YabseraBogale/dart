f2c888ca5db545bc8b8c3f51ac3a44efdae4fee0
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-07-21 15:33:32 +0300

    ok

--- symmetric_difference/bin/symmetric_difference.dart
+++ symmetric_difference/bin/symmetric_difference.dart
@@ -1,4 +1,21 @@
+import 'dart:math';
+
 void main(List<String> arguments) {
-  List<int> array = [1, 2, 3];
-  print(array);
+  var A = GiveRandom(5, 20);
+  var B = GiveRandom(5, 20);
+  var C = GiveRandom(5, 20);
+  print("set 1: $A");
+  print("set 2: $B");
+  print("set 3: $C");
+  print("the difference A-B ${A.difference(B)}");
+  var result = C.difference(A.difference(B));
+  print("the result of (A-B)-C $result");
+}
+
+Set GiveRandom(int LengthOfSet, int RangeOfNumber) {
+  var RandomSetOfNumber = <int>{};
+  for (int i = 0; i < LengthOfSet; i++) {
+    RandomSetOfNumber.add(Random.secure().nextInt(RangeOfNumber));
+  }
+  return RandomSetOfNumber;
 }
