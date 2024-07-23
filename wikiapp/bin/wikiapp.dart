5386ed66add66569e67a6aada1a790f7f7f12fa3
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-07-23 18:05:56 +0300

    ok

--- wikiapp/bin/wikiapp.dart
+++ wikiapp/bin/wikiapp.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:wikiapp/wikiapp.dart' as wikiapp;
 
 void main(List<String> arguments) {
-  print('Hello world: ${wikiapp.calculate()}!');
+  final api =
+      "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
+
+  stdin.readLineSync();
 }
