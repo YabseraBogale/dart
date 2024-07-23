f90ae550b78276378ec9ec4c82f49c5e6154b271
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-07-23 19:51:31 +0300

    ok

--- wikiapp/bin/wikiapp.dart
+++ wikiapp/bin/wikiapp.dart
@@ -1,16 +1,18 @@
 import 'dart:convert';
-import 'dart:io';
 import 'package:http/http.dart' as http;
-import 'package:wikiapp/wikiapp.dart' as wikiapp;
 
 void main(List<String> arguments) async {
   String api =
       "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
-  print("Enter search item");
-  String? search = stdin.readLineSync();
-  final uri = api + search.toString().replaceAll(" ", "&");
+  // print("Enter search item");
+  // String? search = stdin.readLineSync();
+  // final uri = api + search.toString().replaceAll(" ", "&");
+  final uri = "${api}black";
   final httprequest = Uri.parse(uri);
   final httpinfo = await http.read(httprequest);
   final httpjson = json.decode(httpinfo) as Map<String, dynamic>;
-  print(httpjson["query"]);
+  print(httpjson["query"]
+      .toString()
+      .replaceAll("{pages: ", "")
+      .replaceAll("}}}", "}}"));
 }
