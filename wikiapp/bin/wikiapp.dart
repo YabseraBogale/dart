8de55fc7bfc92516262699f769937a04b25b2e87
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-07-23 18:14:00 +0300

    ok

--- wikiapp/bin/wikiapp.dart
+++ wikiapp/bin/wikiapp.dart
@@ -2,9 +2,13 @@ import 'dart:io';
 
 import 'package:wikiapp/wikiapp.dart' as wikiapp;
 
-void main(List<String> arguments) {
-  final api =
+void main(List<String> arguments) async {
+  String api =
       "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
-
-  stdin.readLineSync();
+  print("Enter search item");
+  String? search = stdin.readLineSync();
+  final uri = api + search.toString().replaceAll(" ", "&");
+  final httprequest = Uri.parse(uri);
+  final httpinfo = await http.read(httprequest);
+  print(httpinfo);
 }
