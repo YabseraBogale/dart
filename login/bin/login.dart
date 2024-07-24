e1e04038b5d689cb11cd50ed20ea3c3812b0a43e
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-07-24 22:33:17 +0300

    ok

--- login/bin/login.dart
+++ login/bin/login.dart
@@ -1,1 +1,12 @@
-void main(List<String> arguments) {}
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+void main(List<String> arguments) {
+  final respone = http.post(Uri.parse("https://stmu.edu.pk/wp-login.php"),
+      headers: <String, String>{
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: jsonEncode(<String, String>{"log": "admin", "pwd": "admin"}));
+  print(respone);
+}
