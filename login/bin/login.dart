8c0703756acb2956700ce00c64f4a56d9380113d
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-07-24 22:34:58 +0300

    ok

--- login/bin/login.dart
+++ login/bin/login.dart
@@ -2,11 +2,13 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-void main(List<String> arguments) {
-  final respone = http.post(Uri.parse("https://stmu.edu.pk/wp-login.php"),
-      headers: <String, String>{
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: jsonEncode(<String, String>{"log": "admin", "pwd": "admin"}));
+void main() async {
+  final respone = await http
+      .post(Uri.parse("https://stmu.edu.pk/wp-login.php"),
+          headers: <String, String>{
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: jsonEncode(<String, String>{"log": "admin", "pwd": "admin"}))
+      .whenComplete(action);
   print(respone);
 }
