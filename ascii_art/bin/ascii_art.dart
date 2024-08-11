6e505fe10a16edfa8a9f959b24c702a90d5ca8c0
YabseraBogale <yabserapython@gmail.com>
2024-08-11 19:24:18 +0300

    ok

---
+++ ascii_art/bin/ascii_art.dart
@@ -0,0 +1,5 @@
+import 'package:ascii_art/ascii_art.dart' as ascii_art;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ascii_art.calculate()}!');
+}
---
+++ ascii_art/test/ascii_art_test.dart
@@ -0,0 +1,8 @@
+import 'package:ascii_art/ascii_art.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
