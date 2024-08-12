2f32bbc49f2b3afcaefda06d64da3f811ce7f2ea
YabseraBogale <yabserapython@gmail.com>
2024-08-12 15:30:33 +0300

    ok

--- ascii_art/bin/ascii_art.dart
+++ ascii_art/bin/ascii_art.dart
@@ -2,5 +2,9 @@ import 'package:image/image.dart';
 
 void main() async {
   Image? image = await decodePngFile("wall.png");
-  print("the length ${image?.length}");
+  for (var x = 0; x < (image?.data?.maxChannelValue ?? 0); x++) {
+    for (var y = 0; y < (image?.data?.maxChannelValue ?? 0); y++) {
+      print(image?.getPixel(x, y));
+    }
+  }
 }
