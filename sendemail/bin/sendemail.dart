d1331c1a6873b4e9619c22cb843201db3ca44c82
YabseraBogale <yabserapython@gmail.com>
2024-11-12 10:21:30 +0300

    ok

--- sendemail/bin/sendemail.dart
+++ sendemail/bin/sendemail.dart
@@ -1,14 +1,32 @@
 import 'dart:io';
 import 'package:sqlite3/sqlite3.dart';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async{
   final db = sqlite3.open("gold.db");
-  File("index.html").readAsString().then((String content) {
-    print(content);
+  var index = File("index.html").readAsString().then((String content) {
+    return content;
   });
+  final smtpServer=gmail("cheretaaddis@gmail.com","zgzd xtlt emlc tzfb");
   final results =
       db.select("select * from userdata where Country='Angola' limit 10");
   for (final result in results) {
-    print("${result['Email']} ${result['Sent']} ${result['Country']}");
+     final message = Message()
+    ..from = Address("cheretaaddis@gmail.com", 'YabseraBogale')
+    ..recipients.add(result['Email'])
+    ..subject = 'YabseraBogale Software Developer'
+    ..text = 'This is the plain text.\nThis is line 2 of the text part.'
+    ..html = index.toString();
+
+  try {
+    final sendReport = await send(message, smtpServer);
+    print('Message sent: ${sendReport.toString()}');
+  } on MailerException catch (e) {
+    print('Message not sent.');
+    for (var p in e.problems) {
+      print('Problem: ${p.code}: ${p.msg}');
+    }
+  }
   }
 }
