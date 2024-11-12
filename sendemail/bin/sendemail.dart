dff8488ef677c866863ba6e293bda6c516381d44
YabseraBogale <yabserapython@gmail.com>
2024-11-12 09:56:35 +0300

    ok

--- sendemail/bin/sendemail.dart
+++ sendemail/bin/sendemail.dart
@@ -1,7 +1,14 @@
 import 'dart:io';
+import 'package:sqlite3/sqlite3.dart';
 
 void main(List<String> arguments) {
+  final db = sqlite3.open("gold.db");
   File("index.html").readAsString().then((String content) {
     print(content);
   });
+  final results =
+      db.select("select * from userdata where Country='Angola' limit 10");
+  for (final result in results) {
+    print("${result['Email']} ${result['Sent']} ${result['Country']}");
+  }
 }
