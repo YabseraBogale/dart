b85955a7a1be0eed3dee1ad86915f1af7f7fa38d
YabseraBogale <yabserapython@gmail.com>
2024-11-15 10:31:44 +0300

    ok

---
+++ web_page_enumerator/bin/web_page_enumerator.dart
@@ -0,0 +1,5 @@
+import 'package:web_page_enumerator/web_page_enumerator.dart' as web_page_enumerator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${web_page_enumerator.calculate()}!');
+}
