ed6aebd63651428fd6850d07334681734c2ff9a0
YabseraBogale <yabserapython@gmail.com>
2024-11-15 19:03:57 +0300

    ok

---
+++ server_scanner/bin/server_scanner.dart
@@ -0,0 +1,5 @@
+import 'package:server_scanner/server_scanner.dart' as server_scanner;
+
+void main(List<String> arguments) {
+  print('Hello world: ${server_scanner.calculate()}!');
+}
---
+++ server_scanner/test/server_scanner_test.dart
@@ -0,0 +1,8 @@
+import 'package:server_scanner/server_scanner.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
