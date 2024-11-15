86c947cb85e03f1c3632be71128eae14d0403370
YabseraBogale <yabserapython@gmail.com>
2024-11-15 19:03:08 +0300

    ok

--- web_page_enumerator/bin/web_page_enumerator.dart
+++
@@ -1,5 +0,0 @@
-import 'package:web_page_enumerator/web_page_enumerator.dart' as web_page_enumerator;
-
-void main(List<String> arguments) {
-  print('Hello world: ${web_page_enumerator.calculate()}!');
-}
--- web_page_enumerator/test/web_page_enumerator_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:web_page_enumerator/web_page_enumerator.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
