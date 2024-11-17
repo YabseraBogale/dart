27234a8f038e1fe60aeebe597faef2e6a7b2bb8f
YabseraBogale <yabserapython@gmail.com>
2024-11-17 15:42:26 +0300

    ok

--- nosql/bin/nosql.dart
+++ nosql/bin/nosql.dart
@@ -1,4 +1,20 @@
+import 'package:sembast/sembast_io.dart';
+import 'dart:async';
 
-void main(List<String> arguments) {
+Future main(List<String> arguments) async{
+  final db=await databaseFactoryIo.openDatabase("database.db");
+  var store = intMapStoreFactory.store('my_store');
+
+  var key = await store.add(db, {'name': 'ugly'});
+  var record = await (store.record(key).getSnapshot(db)
+      as FutureOr<RecordSnapshot<int, Map<String, Object>>>);
+  record =
+      (await store.find(db, finder: Finder(filter: Filter.byKey(record.key))))
+          .first as RecordSnapshot<int, Map<String, Object>>;
+  print(record);
+  var records = (await (store.find(db,
+          finder: Finder(filter: Filter.matches('name', '^ugly')))
+      as FutureOr<List<RecordSnapshot<int, Map<String, Object>>>>));
+  print(records);
 
 }
