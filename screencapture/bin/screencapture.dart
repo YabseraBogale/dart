c4a2d3e3b7e16f2a885179cf83da2a1639b608e4
YabseraBogale <yabserapython@gmail.com>
2024-11-21 19:08:07 +0300

    ok

---
+++ screencapture/bin/screencapture.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() async {
+  if (Platform.isWindows) {
+    await Process.run('snippingtool', ['/clip']);
+    print('Screenshot captured on Windows!');
+  } else if (Platform.isMacOS) {
+    await Process.run('screencapture', ['-x', 'screenshot.png']);
+    print('Screenshot saved as screenshot.png on macOS!');
+  } else if (Platform.isLinux) {
+    await Process.run('import', ['-window', 'root', 'screenshot.png']);
+    print('Screenshot saved as screenshot.png on Linux!');
+  } else {
+    print('Platform not supported for screen capture.');
+  }
+}
---
+++ screencapture/test/screencapture_test.dart
@@ -0,0 +1,8 @@
+import 'package:screencapture/screencapture.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
