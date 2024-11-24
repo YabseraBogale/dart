49e7453cb952a9c812a09cbdffd5fb0c171f2fd0
YabseraBogale <yabserapython@gmail.com>
2024-11-24 13:32:01 +0300

    ok

---
+++ fileseeer/bin/fileseeer.dart
@@ -0,0 +1,5 @@
+import 'package:fileseeer/fileseeer.dart' as fileseeer;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fileseeer.calculate()}!');
+}
---
+++ fileseeer/test/fileseeer_test.dart
@@ -0,0 +1,8 @@
+import 'package:fileseeer/fileseeer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
