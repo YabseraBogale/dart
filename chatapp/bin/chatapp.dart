5419c94d36722d84e6048096eff275c9bc6894bd
YabseraBogale <yabserapython@gmail.com>
2024-11-24 10:56:12 +0300

    ok

--- chatapp/bin/chatapp.dart
+++ chatapp/bin/chatapp.dart
@@ -1,5 +1,38 @@
-import 'package:chatapp/chatapp.dart' as chatapp;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${chatapp.calculate()}!');
+void main() async{
+  var server= await HttpServer.bind(InternetAddress.anyIPv4,8080);
+  print("server starting at localhost:${server.port}");
+  List<WebSocket> clients=[];
+
+  await for (HttpRequest request in server){
+    if(request.uri.path=="/ws" && WebSocketTransformer.isUpgradeRequest(request)){
+      WebSocket socket= await WebSocketTransformer.upgrade(request);
+      clients.add(socket);
+      print("Client connected: ${clients.length} total");
+      socket.listen(
+             (message){
+               print("Recived $message");
+              for(var client in clients){
+                if(client!=socket){
+                  client.add(message);
+                }
+              }
+
+            },
+            onDone: (){
+              print("Client Disconnected");
+              clients.remove(socket);
+            },
+            onError: (error){
+              print("Error: $error");
+            }
+      );
+    } else{
+      request.response.statusCode=HttpStatus.notFound;
+      await request.response.close();
+    }
+  }
+
+
 }
