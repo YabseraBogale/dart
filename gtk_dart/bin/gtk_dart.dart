1e40f772c043dde35f5cda0648124011aca5c6b3
YabseraBogale <yabserapython@gmail.com>
2024-12-03 17:47:23 +0300

    ok

---
+++ gtk_dart/bin/gtk_dart.dart
@@ -0,0 +1,5 @@
+import 'package:gtk_dart/gtk_dart.dart' as gtk_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gtk_dart.calculate()}!');
+}
---
+++ gtk_dart/test/gtk_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:gtk_dart/gtk_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
