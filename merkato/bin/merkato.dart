832932df9749ca183f084ce074f52b79e82ccd90
YabseraBogale <yabserapython@gmail.com>
2024-12-10 22:15:34 +0300

    ok

--- merkato/bin/merkato.dart
+++ merkato/bin/merkato.dart
@@ -1,24 +1,38 @@
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart';
 import 'package:sqlite3/sqlite3.dart';
-import 'dart:io';
+
 void main() async{
   final db=sqlite3.open("merkato.db");
-  var a=[];
   final regx=RegExp(r"directory/\d+-",multiLine: true);
- for(var i=1;i<53;i++){
+  final stmt=db.prepare("Insert into Company(CompanyName,CompanyLink,CompanyInformation) values(?,?,?)");
+  for(var i=1;i<53;i++){
    try{
-     print("Number of links collected ${a.length} and i at $i");
+     print("Number of links collected at $i");
      if(i==1){
        var merkato=await http.get(Uri.parse("https://www.2merkato.com/directory/"));
        if(merkato.statusCode==200){
+
          final document=parse(merkato.body);
-         final links=document.querySelectorAll("h4");
+         final links=document.querySelectorAll("h4").map((a)=>a.innerHtml);
+
          for(var link in links){
-           if(regx.hasMatch(link.innerHtml)){
-             a.add(link.innerHtml);
+           if(regx.hasMatch(link)){
+              var companyName=parse(link).querySelectorAll("a").map((a)=>{a.text}).first.first;
+              var companyLink=parse(link).querySelectorAll("a").map((a)=>{a.attributes['href']}).first.first;
+              final content=await http.get(Uri.parse("https://www.2merkato.com$companyLink"));
+              var companyInformation=parse(content.body)
+                                          .querySelectorAll(".table-condensed")
+                                          .map((a)=>a.text)
+                                          .first
+                                          .trim()
+                                          .replaceAll("\n","")
+                                          .replaceAll("\t","");
+              stmt.execute([companyName,"https://www.2merkato.com$companyLink",companyInformation]);
+
            }
          }
+
        } else{
          print("connection response ${merkato.statusCode}");
        }
@@ -26,10 +40,21 @@ void main() async{
        var merkato=await http.get(Uri.parse("https://www.2merkato.com/directory/page:$i"));
        if(merkato.statusCode==200){
          final document=parse(merkato.body);
-         final links=document.querySelectorAll("h4");
+         final links=document.querySelectorAll("h4").map((a)=>a.innerHtml);
          for(var link in links){
-           if(regx.hasMatch(link.innerHtml)){
-             a.add(link.innerHtml);
+           if(regx.hasMatch(link)){
+             var companyName=parse(link).querySelectorAll("a").map((a)=>{a.text}).first.first;
+             var companyLink=parse(link).querySelectorAll("a").map((a)=>{a.attributes['href']}).first.first;
+             final content=await http.get(Uri.parse("https://www.2merkato.com$companyLink"));
+             var companyInformation=parse(content.body)
+                                          .querySelectorAll(".table-condensed")
+                                          .map((a)=>a.text)
+                                          .first
+                                          .trim()
+                                          .replaceAll("\n","")
+                                          .replaceAll("\t","");
+             stmt.execute([companyName,"https://www.2merkato.com$companyLink",companyInformation]);
+
            }
          }
        } else{
@@ -41,18 +66,6 @@ void main() async{
   }
 }
 
-for(var i=0;i<a.length;i++){
-  print("data at $i");
-  try{
-    final data=parse(a[i]);
-    final stmt=db.prepare("Insert into Company(CompanyName,CompanyLink,CompanyInformation) values(?,?,'not yet')");
-    stmt.execute([data.text,data.attributes['href']]);
-    if(i%100==0 && i!=0){
-      sleep(Duration(seconds: 3));
-    }
-  }catch(e){
-    print(e);
-  }
-}
+
 
 }
