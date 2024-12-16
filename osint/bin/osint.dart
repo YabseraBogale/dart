7d95fd19426920affb1dfbeebe31d44bb0062f0e
YabseraBogale <yabserapython@gmail.com>
2024-12-16 19:00:07 +0300

    ok

--- osint/bin/osint.dart
+++ osint/bin/osint.dart
@@ -1,5 +1,15 @@
+import 'package:sqlite3/sqlite3.dart';
+//import 'package:http/http.dart' as http;
+//import 'package:html/parser.dart';
 
-
-void main(List<String> arguments) {
-
+void main(List<String> arguments) async{
+    try{
+        final db=sqlite3.open("merkato.db");
+        final result=db.select("select Link from Company where Link!='null'");
+        for(final row in result){
+          print(row['Link']);
+        }
+    } catch(e){
+      print(e);
+    }
 }
