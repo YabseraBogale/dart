a4c0c758f8f292dda11298ed272e3b4f16fcd783
YabseraBogale <yabserapython@gmail.com>
2024-12-18 18:09:50 +0300

    ok

--- osint/bin/osint.dart
+++ osint/bin/osint.dart
@@ -3,18 +3,21 @@ import 'package:http/http.dart' as http;
 //import 'package:html/parser.dart';
 
 void main(List<String> arguments) async{
-    try{
+
         final db=sqlite3.open("merkato.db");
         final result=db.select("select Link from Company where Link!='null'");
         for(final row in result){
-          var website=await http.get(Uri.parse(row['Link']));
-          if(website.statusCode!=200){
-            print("Link: ${row['Link']}, statusCode: ${website.statusCode}");
-          }else{
-            print("Link: ${row['Link']}, statusCode: ${website.statusCode}");
+
+          try{
+            var website=await http.get(Uri.parse(row['Link'].toString().replaceAll("www.","")));
+            if(website.statusCode!=200){
+              print("Link: ${row['Link']}, statusCode: ${website.statusCode}");
+            }else{
+              print(website.headers);
+            }
+          } catch(e){
+            print(e);
           }
         }
-    } catch(e){
-      print(e);
-    }
+
 }
