6854c976a4f2ddd8f9a5139f59583d382d1d6f07
YabseraBogale <yabserapython@gmail.com>
2024-12-20 10:04:23 +0300

    ok

--- osint/bin/osint.dart
+++ osint/bin/osint.dart
@@ -7,23 +7,7 @@ void main(List<String> arguments) async{
         final db=sqlite3.open("merkato.db");
         final result=db.select("select Link from Company where Link!='null'");
         for(final row in result){
-
-          try{
-            var website=await http.get(Uri.parse(row['Link'].toString().replaceAll("www.","")));
-            if(website.statusCode!=200){
-              print("Link: ${row['Link']}, statusCode: ${website.statusCode}");
-            }else{
-              try{
-                final stmt=db.prepare("Update Company Set ServerInfo=? where Link=?");
-                stmt.execute([website.headers.toString(),row['Link']]);
-              }catch(e){
-                print(e);
-
-              }
-            }
-          } catch(e){
-            print(e);
-          }
+          print(row['Link']);
         }
 
 }
