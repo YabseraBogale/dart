4c898297530bf3d4f27074948f96bbdec03fe8d3
YabseraBogale <yabserapython@gmail.com>
2025-03-20 12:45:46 +0300

    oj

--- metadata/bin/metadata.dart
+++ metadata/bin/metadata.dart
@@ -1,7 +1,14 @@
 import 'dart:io';
+import 'package:shelf/shelf.dart';
+import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
 
 void main(List<String> arguments) async {
-  var file = File("pubspec.lock");
-  var length = await file.length();
-  print(length);
+  final router = Router();
+  router.get("/", (Request req) {
+    return Response.ok("Hello World");
+  });
+  final handler = Pipeline().addMiddleware(logRequests()).addHandler(router);
+  final server = await shelf_io.serve(handler, InternetAddress.anyIPv4, 8080);
+  print("Serving at http://${server.address.host}:${server.port}");
 }
