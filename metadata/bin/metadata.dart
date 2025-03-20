320afb1ac78b96261f56a42024fbe57fe9b8ebbc
YabseraBogale <yabserapython@gmail.com>
2025-03-20 13:47:12 +0300

    hot reload sucks

--- metadata/bin/metadata.dart
+++ metadata/bin/metadata.dart
@@ -2,11 +2,19 @@ import 'dart:io';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_static/shelf_static.dart';
 
 void main(List<String> arguments) async {
   final router = Router();
+  final staticHandler = createStaticHandler(
+    "public",
+    defaultDocument: "index.html",
+  );
   router.get("/", (Request req) {
-    return Response.ok("Hello World");
+    return staticHandler(req);
+  });
+  router.get("/go", (Request req) {
+    return staticHandler(req);
   });
   final handler = Pipeline().addMiddleware(logRequests()).addHandler(router);
   final server = await shelf_io.serve(handler, InternetAddress.anyIPv4, 8080);
