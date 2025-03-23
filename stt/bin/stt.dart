b37dbc48c5301d808e56ddb8d25fb47f84acb1a7
YabseraBogale <yabserapython@gmail.com>
2025-03-23 12:24:28 +0300

    sound

---
+++ stt/bin/stt.dart
@@ -0,0 +1,5 @@
+import 'package:stt/stt.dart' as stt;
+
+void main(List<String> arguments) {
+  print('Hello world: ${stt.calculate()}!');
+}
---
+++ stt/test/stt_test.dart
@@ -0,0 +1,8 @@
+import 'package:stt/stt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
